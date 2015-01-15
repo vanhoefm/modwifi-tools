72e9b0ca3c5cc1f2e37de72a0b28ff5497450c24
Mathy <vanhoefm@users.noreply.github.com>
2015-01-14 21:22:10 -0500

    constantjam: improved user interface

--- constantjam.cpp
+++ constantjam.cpp
@@ -3,32 +3,37 @@
 
 #include "osal_wi.h"
 
-const char warning[] =
-	"Warning:\n"
-	"- Some devices stop operating properly when in monitor mode\n"
-	"  and attempting to transmit packets while you are jamming!\n"
-	"  Setting force_channel_idle may help if you experience problems.\n"
-	"- Some devices stop recieving frames after being jammed for a long time.\n"
-	"  Perhaps because they configured an unrealisticly high noise-floor level.\n"
-	"  Plug the device you jammed in/out if it no longer seems to be working!\n\n";
+
+char usage[] =
+
+"\n"
+"  constantjam - Mathy Vanhoef\n"
+"\n"
+"  usage: constantjam interface [channel]\n"
+"\n"
+"    Warnings:\n"
+"    - Some devices stop operating properly when in monitor mode\n"
+"      and attempting to transmit packets while you are jamming!\n"
+"      Setting force_channel_idle may help if you experience problems.\n"
+"    - Some devices stop recieving frames after being jammed for a long time.\n"
+"      Perhaps because they configured an unrealisticly high noise-floor level.\n"
+"      Reset the device you jammed if it no longer seems to be working!\n";
 
 int main(int argc, char *argv[])
 {
 	wi_dev dev;
 
-	if (argc != 3) {
-		printf("Usage: contjam interface channel\n");
+	if (argc < 2) {
+		puts(usage);
 		return 1;
 	}
 
 	if (osal_wi_open(argv[1], &dev) < 0) return 1;
+	if (argc >= 3) osal_wi_setchannel(&dev, atoi(argv[2]));
 
-	printf("%s", warning);
-
-	osal_wi_setchannel(&dev, atoi(argv[2]));
 	osal_wi_constantjam_start(&dev);
 
-	printf("Press any key to stop jamming...\n");
+	printf("Jamming channel %d. Press any key to stop ...\n", osal_wi_getchannel(&dev));
 	getchar();
 
 	osal_wi_constantjam_stop(&dev);
