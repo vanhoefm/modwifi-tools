ce726674518de8d10098901526689fa80468f13c
Rot127 <bla.52@gmx.net>
2020-08-09 14:11:05 +0200

    add feature: configure only one interface

--- config_dongle.sh
+++ config_dongle.sh
@@ -5,22 +5,40 @@ print_exec() {
 	eval $1
 }
 
+config_iface() {
+	print_exec "ip link set $1 down"
+	print_exec "iw $1 set type monitor"
+	print_exec "iw $1 set monitor fcsfail"
+}
 
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
 	echo -e "$0\
 		\n\t-k		Kill all processes listed by airmon-zc\
 		\n\t-d [phyX]	Disable Carrier Sense for physical device phyX\
+		\n\t-i [iface]	Configure only interface [iface]\
 		\n\t-h		Show this help"
 	exit
 fi
 
-while getopts d:h:k option; do
-	case "$option" in
+while getopts "d:k:i:h" option; do
+	case ${option} in
 	    d) PHY=${OPTARG}
-		    NOCS=true;;
+		    NOCS=true
+		    ;;
 	    k) KILL=true;;
+	    i) ONLY_ONE_IFACE=true
+		    IFACE=${OPTARG}
+		    ;;
+
 	esac
 done
+
+if [[ $NOCS ]]; then
+	echo -e "\nDisable Carrier Sense"
+    print_exec "echo 1 > /sys/kernel/debug/ieee80211/$PHY/ath9k_htc/registers/force_channel_idle"
+    print_exec "echo 1 > /sys/kernel/debug/ieee80211/$PHY/ath9k_htc/registers/ignore_virt_cs"
+    exit
+fi
 
 if [[ $KILL ]]; then
 	echo airmon-zc check
@@ -37,14 +55,13 @@ fi
 echo -e "\nConfigure intrefaces"
 
 ifnames=$(iw dev | grep -E -o "Interface (.+)" | grep -E -o "\s.+$" | grep -E -o "[a-z0-9]+")
-if [[ $ifnames == "" ]]; then
-	echo "No interfaces detected"
-	exit
-fi
 echo "$ifnames" | while IFS= read -r interface; do
-	print_exec "ip link set $interface down"
-	print_exec "iw $interface set type monitor"
-	print_exec "iw $interface set monitor fcsfail"
+	if [[ ! $ONLY_ONE_IFACE ]]; then
+		config_iface $interface
+	elif [[ $IFACE == $interface ]]; then
+		config_iface $interface
+		break
+	fi
 done
 
 echo -e "\nEnable full debug output for dmesg"
@@ -53,11 +70,9 @@ echo "$phynum" | while IFS= read -r num; do
 	print_exec "echo 0xffffffff > /sys/kernel/debug/ieee80211/phy$num/ath9k_htc/debug"
 done
 
-if [[ $NOCS ]]; then
-	echo -e "\nDisable Carrier Sense"
-    print_exec "echo 1 > /sys/kernel/debug/ieee80211/$PHY/ath9k_htc/registers/force_channel_idle"
-    print_exec "echo 1 > /sys/kernel/debug/ieee80211/$PHY/ath9k_htc/registers/ignore_virt_cs"
-fi
-
 echo -e "\n\nCheck config"
-iw dev
+if [[ $ONLY_ONE_IFACE ]]; then
+	iw $IFACE info
+else
+	iw dev
+fi
